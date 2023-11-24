f9f1c738d09daf6e4e1f9a3efbe8c6e2e2341433
PyTruM <81774370+PyTruM@users.noreply.github.com>
2023-11-24 20:58:22 +0400

    Update python.py

--- python.py
+++ python.py
@@ -5,14 +5,23 @@ def check_url(url: str, count: int):
     list = []
     with open('check_url.txt', 'a+') as f:
         for i in range(count):
-            req_url = req.get(url)
-            r = req_url.status_code
-            t = round(req_url.elapsed.total_seconds(), 5)
-            list.append(t)
-            f.writelines(f'Статус ответа: {r}, Время ответа: {t} \n')
+            try:
+                req_url = req.get(url)
+                r = req_url.status_code
+                t = round(req_url.elapsed.total_seconds(), 5)
+                list.append(t)
+                f.writelines(f'Статус ответа: {r}, Время ответа: {t} \n')
+            except req.ConnectionError as e:
+                print("Ошибка подключения:", e)
+            except req.Timeout as e:
+                print("Ошибка тайм-аута:", e)
+            except req.RequestException as e:
+                print("Ошибка запроса:", e)
         list_avg = round(mean(list), 5)
         f.write(f'Среднее время ответа: {list_avg} \n')
         f.write('-'*20+'\n')
 
+
+url = 'https://www.google.com'
 check_url(url, 3)
 
